bffaf9f6b61a95f8521a0f1f22320d5da6125d45
mlvzk <mlvzk@protonmail.com>
2022-05-26 15:28:44 +0200

    perf: speed up acquiring jobs

--- pgqueue/init-lock-conn.sql
+++ pgqueue/init-lock-conn.sql
@@ -2,12 +2,15 @@ CREATE TEMPORARY TABLE acquired_jobs (job_id bigint not null, queue text not nul
 
 CREATE FUNCTION pg_temp.acquire_jobs(text, int) RETURNS SETOF pgqueue.jobs AS $$
 DECLARE
-    jid bigint;
     acquired_job_ids bigint[];
-    counter int;
+    owned_lock_count int;
+    expected_lock_count int;
 BEGIN
-    counter = 0;
-    FOR jid IN
+    PERFORM pg_temp.define_lock_max();
+
+    SELECT count(*) INTO owned_lock_count FROM pg_locks WHERE locktype = 'advisory' AND pid = pg_backend_pid();
+
+    WITH available_jobs AS (
         SELECT j.id
         FROM pgqueue.jobs AS j
         WHERE queue = $1
@@ -16,20 +19,48 @@ BEGIN
         AND (expires_at IS NULL OR now() < expires_at)
         AND (failed_at IS NULL OR retryable = true)
         ORDER BY priority DESC, run_at NULLS LAST, expires_at NULLS LAST, id
-    LOOP
-        IF pg_try_advisory_lock(jid) THEN
-            counter = counter + 1;
-            acquired_job_ids = acquired_job_ids || jid;
-        END IF;
-        IF counter >= $2 THEN
-            EXIT;
-        END IF;
-    END LOOP;
+    ) SELECT array_agg(j.id) INTO acquired_job_ids FROM available_jobs j WHERE pg_temp.lock_max(j.id, $2) LIMIT $2;
+
+    expected_lock_count = owned_lock_count + array_length(acquired_job_ids, 1);
+
+    SELECT count(*) INTO owned_lock_count FROM pg_locks WHERE locktype = 'advisory' AND pid = pg_backend_pid();
+
+    IF owned_lock_count != expected_lock_count THEN
+        RAISE EXCEPTION 'Expected % to be the total lock count, but got %', expected_lock_count, owned_lock_count;
+    END IF;
 
     INSERT INTO acquired_jobs SELECT unnest(acquired_job_ids), $1;
     RETURN QUERY SELECT * FROM pgqueue.jobs WHERE id = ANY(acquired_job_ids);
+    PERFORM pg_advisory_lock(jid) FROM unnest(acquired_job_ids) jid;
 END
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION pg_temp.define_lock_max() RETURNS void AS $LOCKMAX$
+BEGIN
+    PERFORM set_config('lock_max.locked', '0', true);
+
+    CREATE OR REPLACE FUNCTION pg_temp.lock_max(id bigint, max_locks int) RETURNS boolean AS $$
+    DECLARE
+        locked boolean;
+        lock_count int;
+    BEGIN
+        lock_count = current_setting('lock_max.locked');
+
+        IF lock_count >= max_locks THEN
+            CREATE OR REPLACE FUNCTION pg_temp.lock_max(id bigint, max_locks int) RETURNS boolean AS 'begin return false; end;' LANGUAGE plpgsql;
+            RETURN false;
+        END IF;
+
+        locked = pg_try_advisory_xact_lock(id);
+        IF locked THEN
+            PERFORM set_config('lock_max.locked', (lock_count + 1)::text, true);
+            RETURN true;
+        END IF;
+        RETURN false;
+    END;
+    $$ LANGUAGE plpgsql;
+END
+$LOCKMAX$ LANGUAGE plpgsql;
 
 CREATE FUNCTION pg_temp.unlock_jobs(bigint[]) RETURNS void AS $$
 BEGIN
