4e53a00601de6144404302c34a220a0045421331
mlvzk <mlvzk@protonmail.com>
2022-05-27 13:50:57 +0200

    fix: match more dead jobs in clean up DELETE

--- pgqueue/clean-up.sql
+++ pgqueue/clean-up.sql
@@ -1,7 +1,18 @@
-DELETE FROM pgqueue.jobs WHERE NOT (
-    (run_at IS NULL OR run_at <= now())
-	AND (expires_at IS NULL OR now() < expires_at)
-	AND (failed_at IS NULL OR retryable = true)
-) AND (failed_at < now() - INTERVAL '1 hour')
-	AND (expires_at IS NULL OR expires_at < now() - INTERVAL '1 hour')
-	AND id NOT IN (SELECT (classid::bigint << 32) + objid::bigint FROM pg_locks WHERE locktype = 'advisory');
+DELETE
+FROM pgqueue.jobs
+WHERE
+	(
+		(
+			expires_at IS NOT NULL
+			AND expires_at < now() - INTERVAL '1 hour'
+		)
+		OR
+		(
+			failed_at IS NOT NULL
+			AND failed_at < now() - INTERVAL '1 hour'
+			AND retryable = false
+		)
+	)
+	AND id NOT IN (
+		SELECT (classid::bigint << 32) + objid::bigint FROM pg_locks WHERE locktype = 'advisory'
+	);
