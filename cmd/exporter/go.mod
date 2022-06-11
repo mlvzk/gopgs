7052e56e24b17cb6892c0291ac11b314dd9a964f
mlvzk <mlvzk@protonmail.com>
2022-06-11 10:57:34 +0200

    feat: create a prometheus exporter

--- cmd/exporter/go.mod
+++ cmd/exporter/go.mod
@@ -1,21 +1,16 @@
-module github.com/mlvzk/gopgs
+module github.com/mlvzk/gopgs/cmd/exporter
 
 go 1.18
 
 require (
 	github.com/jackc/pgx/v4 v4.16.1
-	github.com/valyala/gozstd v1.17.0
-	go.opentelemetry.io/otel/trace v1.7.0
+	github.com/prometheus/client_golang v1.12.2
 )
 
 require (
-	github.com/go-logr/logr v1.2.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-)
-
-require (
-	github.com/BurntSushi/locker v0.0.0-20171006230638-a6e239ea1c69
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -24,7 +19,12 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.11.0 // indirect
 	github.com/jackc/puddle v1.2.1 // indirect
-	go.opentelemetry.io/otel v1.7.0
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
---
+++ cmd/exporter/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	pgqUrl := os.Getenv("PGQUEUE_URL")
+	if pgqUrl == "" {
+		panic("PGQUEUE_URL is not set")
+	}
+
+	db, err := pgxpool.Connect(ctx, pgqUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	jobsGauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "pgqueue",
+		Name:      "jobs",
+	}, []string{"queue"})
+
+	prometheus.MustRegister(jobsGauge)
+
+	go func() {
+		for {
+			rows, err := db.Query(ctx, `
+			SELECT queue, count(*)
+			FROM pgqueue.jobs AS j
+			WHERE (run_at IS NULL OR run_at <= now())
+			AND (expires_at IS NULL OR now() < expires_at)
+			AND (failed_at IS NULL OR retryable = true)
+			GROUP BY queue
+			`)
+			if err != nil {
+				panic(err)
+			}
+
+			for rows.Next() {
+				var queue string
+				var count int
+				if err := rows.Scan(&queue, &count); err != nil {
+					panic(err)
+				}
+
+				jobsGauge.WithLabelValues(queue).Set(float64(count))
+			}
+
+			if err := rows.Err(); err != nil {
+				panic(err)
+			}
+
+			rows.Close()
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(time.Minute):
+			}
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":7688", nil)
+}
